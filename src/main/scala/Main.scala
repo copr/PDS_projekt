82baf2615d062a7bf11ac4de3dc178f963db2e70
Martin Kobersky <martinkobersky@gmail.com>
2015-03-25 02:28:30 +0100

    Pridane spusteni vypoctu text ranku pomoci monte carla

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,13 +1,21 @@
 import playingWithActors._
 import akka.actor._
-import textRank.TextRank
-
+import textRank._
 
 object Application {
   def main(args: Array[String]) = {
-    val sentences = scala.io.Source.fromFile("text").mkString
-    val result = TextRank.rank(sentences, 15)
-    result.map(x => println(x._1))
-    val k = 5
+    val nameOfFile = "text"
+    val sentences = scala.io.Source.fromFile(nameOfFile).mkString
+    val result = TextRank.rank(sentences, 35)
+    val system = ActorSystem("mySystem")
+    val master = system.actorOf(Props(new Master(StringParser.wordsFromSentences(sentences))))
+    result.map(x => println(x._3))
+    master ! Start
+    master ! Hello
+
+//    println(result.foldLeft(0.0)((a,b) => a+b._3))
+
+
+    val k = 15
   }
 }
